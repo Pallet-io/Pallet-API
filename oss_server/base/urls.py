ea4115529fc9f87c04302c86a8ef6dc51791c889
Stanley Ding <stanleyding.tw@gmail.com>
2016-07-12 15:04:18 +0800

    Add `mint raw tx` API.

--- oss_server/base/urls.py
+++ oss_server/base/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from .v1.views import (CreateRawTxView,
+                       CreateMintRawTxView,
                        GetBalanceView,
                        GetLicenseInfoView,
                        GetRawTxView,
@@ -9,6 +10,8 @@ from .v1.views import (CreateRawTxView,
 urlpatterns = [
     url('^v1/balance/(?P<address>[a-zA-Z0-9]+)$', GetBalanceView.as_view()),
     url('^v1/license/(?P<color_id>\d+)$', GetLicenseInfoView.as_view()),
+    url('^v1/mint/create$', CreateMintRawTxView.as_view()),
+    url('^v1/mint/send$', SendRawTxView.as_view()),
     url('^v1/transaction/create$', CreateRawTxView.as_view()),
     url('^v1/transaction/send$', SendRawTxView.as_view()),
     url('^v1/transaction/(?P<tx_id>[a-z0-9]+)$', GetRawTxView.as_view()),
