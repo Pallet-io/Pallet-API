c31904953b1280c97b0e16b045ac96df45c4afd7
bun919tw <bun919tw@gmail.com>
2016-07-18 16:21:49 +0800

    Refactor explorer blocks API

--- oss_server/explorer/v1/tests.py
+++ oss_server/explorer/v1/tests.py
@@ -34,18 +34,21 @@ class GetLatestBlocksTest(TestCase):
 
 class GetBlockByHashTest(TestCase):
     def setUp(self):
-        block1 = Block.objects.create(hash='111', tx_count=1)
-        block2 = Block.objects.create(hash='123', prev_block=block1, tx_count=1)
-        block3 = Block.objects.create(hash='333', prev_block=block2, tx_count=1)
+        block1 = Block.objects.create(hash='000004e0223a146664188edebf7efbce82c3a421ce70f30b71c156368c21caaf',
+                                      tx_count=1)
+        block2 = Block.objects.create(hash='00000319eb1fbe75c75e6ad3970855ac67f8687febd230b3c26c074474889d3b',
+                                      prev_block=block1, tx_count=1)
+        block3 = Block.objects.create(hash='0000020897789853ddfa697e3c5b729c34ba10cb722f10147e13e6a0249038dd',
+                                      prev_block=block2, tx_count=1)
 
     def test_get_block_by_hash(self):
-        url = '/explorer/v1/blocks?hash=123'
+        url = '/explorer/v1/blocks/00000319eb1fbe75c75e6ad3970855ac67f8687febd230b3c26c074474889d3b'
         response = self.client.get(url)
         self.assertEqual(response.status_code, httplib.OK)
-        self.assertEqual(response.json()['block']['hash'], '123')
+        self.assertEqual(response.json()['block']['hash'], '00000319eb1fbe75c75e6ad3970855ac67f8687febd230b3c26c074474889d3b')
 
     def test_block_not_found(self):
-        url = '/explorer/v1/blocks?hash=222'
+        url = '/explorer/v1/blocks/00000319eb1fbe75c75e6ad3970855ac67f8687febd230b3c26c074474889d3c'
         response = self.client.get(url)
         self.assertEqual(response.status_code, httplib.NOT_FOUND)
         self.assertEqual(response.json(), {'error': 'block not exist'})
@@ -53,20 +56,24 @@ class GetBlockByHashTest(TestCase):
 
 class GetBlockByHeightTest(TestCase):
     def setUp(self):
-        block1 = Block.objects.create(hash='111', height=0, tx_count=1, in_longest=1)
-        block2 = Block.objects.create(hash='123', height=1, prev_block=block1, tx_count=1, in_longest=1)
-        block3 = Block.objects.create(hash='333', height=2, prev_block=block2, tx_count=1, in_longest=1)
-        block4 = Block.objects.create(hash='444', height=2, prev_block=block2, tx_count=1, in_longest=0)
+        block1 = Block.objects.create(hash='000004e0223a146664188edebf7efbce82c3a421ce70f30b71c156368c21caaf', height=0,
+                                      tx_count=1, in_longest=1)
+        block2 = Block.objects.create(hash='00000319eb1fbe75c75e6ad3970855ac67f8687febd230b3c26c074474889d3b', height=1,
+                                      prev_block=block1, tx_count=1, in_longest=1)
+        block3 = Block.objects.create(hash='0000020897789853ddfa697e3c5b729c34ba10cb722f10147e13e6a0249038dd', height=2,
+                                      prev_block=block2, tx_count=1, in_longest=1)
+        block4 = Block.objects.create(hash='00000808ca8cb51a1c380a972ed2394b66cb9fe814fa898374858291c525d399', height=2,
+                                      prev_block=block2, tx_count=1, in_longest=0)
 
     def test_get_block_by_height(self):
-        url = '/explorer/v1/blocks?height=2'
+        url = '/explorer/v1/blocks/2'
         response = self.client.get(url)
         self.assertEqual(response.status_code, httplib.OK)
         self.assertEqual(int(response.json()['block']['height']), 2)
         self.assertEqual(response.json()['block']['branch'], 'main')
 
     def test_block_not_found(self):
-        url = '/explorer/v1/blocks?height=4'
+        url = '/explorer/v1/blocks/4'
         response = self.client.get(url)
         self.assertEqual(response.status_code, httplib.NOT_FOUND)
         self.assertEqual(response.json(), {'error': 'block not exist'})
