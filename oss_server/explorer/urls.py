5d613784466e991de0102566b27534b59d8a7361
bun919tw <bun919tw@gmail.com>
2016-07-18 16:21:49 +0800

    Refactor explorer blocks API

--- oss_server/explorer/urls.py
+++ oss_server/explorer/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .v1.views import RetrieveBlockView
+from .v1.views import GetLatestBlocksView, GetBlockByHash, GetBlockByHeight
 
 urlpatterns = [
-    url('^v1/blocks$', RetrieveBlockView.as_view()),
+    url('^v1/blocks$', GetLatestBlocksView.as_view()),
+    url('^v1/blocks/(?P<block_hash>[A-Za-z0-9]{64})', GetBlockByHash.as_view()),
+    url('^v1/blocks/(?P<block_height>\d{1,10})', GetBlockByHeight.as_view()),
 ]
--- oss_server/explorer/v1/views.py
+++ oss_server/explorer/v1/views.py
@@ -6,32 +6,29 @@ from django.views.generic import View
 from ..models import *
 
 
-class RetrieveBlockView(View):
+class GetLatestBlocksView(View):
     def get(self, request):
-        if 'hash' in request.GET:
-            return self.get_block_by_hash(request.GET['hash'])
-        elif 'height' in request.GET:
-            return self.get_block_by_height(request.GET['height'])
-        else:
-            return self.get_latest_blocks()
-
-    def get_block_by_hash(self, hash):
-        try:
-            response = {'block': Block.objects.get(hash=hash).as_dict()}
-            return JsonResponse(response)
-        except Block.DoesNotExist:
-            response = {'error': 'block not exist'}
-            return JsonResponse(response, status=httplib.NOT_FOUND)
-
-    def get_block_by_height(self, height):
-        try:
-            response = {'block': Block.objects.get(height=height, in_longest=1).as_dict()}
-            return JsonResponse(response)
-        except Block.DoesNotExist:
-            response = {'error': 'block not exist'}
-            return JsonResponse(response, status=httplib.NOT_FOUND)
-
-    def get_latest_blocks(self):
         latest_blocks = Block.objects.filter(in_longest=1)[:50]
         response = {'blocks': [block.as_dict() for block in latest_blocks]}
         return JsonResponse(response)
+
+
+class GetBlockByHash(View):
+    def get(self, request, block_hash):
+        try:
+            response = {'block': Block.objects.get(hash=block_hash).as_dict()}
+            return JsonResponse(response)
+        except Block.DoesNotExist:
+            response = {'error': 'block not exist'}
+            return JsonResponse(response, status=httplib.NOT_FOUND)
+
+
+class GetBlockByHeight(View):
+    def get(self, request, block_height):
+        try:
+            response = {'block': Block.objects.get(height=block_height, in_longest=1).as_dict()}
+            return JsonResponse(response)
+        except Block.DoesNotExist:
+            response = {'error': 'block not exist'}
+            return JsonResponse(response, status=httplib.NOT_FOUND)
+
