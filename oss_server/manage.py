5a5bd142adec2164e630138348ee19c69ec63d41
Ben <coopermilk123@gmail.com>
2017-04-20 13:47:01 +0800

    move behavior of adapting config file to setting.py and ignore it

--- oss_server/manage.py
+++ oss_server/manage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from oss_server.settings.base import SERVER_CONFIG_ENV
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "<OSS_SETTING_PATH>")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", SERVER_CONFIG_ENV)
 
     from django.core.management import execute_from_command_line
 
