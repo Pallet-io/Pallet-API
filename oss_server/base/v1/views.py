00252175d572c6dc3270af179cbd29f13d07190b
lbyyy <skzlbyyy@gmail.com>
2017-11-19 17:04:53 +0800

    Fix base/v1/general-transaction/prepare

--- oss_server/base/v1/views.py
+++ oss_server/base/v1/views.py
@@ -226,7 +226,7 @@ class GeneralTxView(CsrfExemptMixin, View):
             from_address = tx_info['from_address']
 
             addr_in = tx_info_in.setdefault(from_address, 0)
-            addr_in += tx_info['amount']
+            tx_info_in[from_address] += tx_info['amount']
 
         return tx_info_in
 
@@ -238,7 +238,7 @@ class GeneralTxView(CsrfExemptMixin, View):
             to_address = tx_info['to_address']
 
             addr_in = tx_info_out.setdefault(to_address, 0)
-            addr_in += tx_info['amount']
+            tx_info_out[to_address] += tx_info['amount']
 
         return tx_info_out
 
