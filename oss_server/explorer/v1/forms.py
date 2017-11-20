9176c18492df05c0ec95365aaccb42ae50b19af2
lbyyy <skzlbyyy@gmail.com>
2017-11-20 11:35:39 +0800

    Fix explorer/v1 api for color/type issue
    1. transactions/address/[:address]
    2. addresses/[:address]/balance
    3. addresses/[:address]/utxos

--- oss_server/explorer/v1/forms.py
+++ oss_server/explorer/v1/forms.py
@@ -8,12 +8,6 @@ class GetAddressTxsForm(forms.Form):
                                          'min_length': 'length of `starting_after` should be exactly 64',
                                          'max_length': 'length of `starting_after` should be exactly 64'
                                      })
-    tx_type = forms.IntegerField(required=False, min_value=0,
-                                 error_messages={
-                                     'invalid': '`tx_type` is invalid',
-                                     'min_value': '`tx_type` should be greater than or equal to %(limit_value)s',
-                                     'max_value': '`tx_type` should be less than or equal to %(limit_value)s'
-                                 })
     since = forms.IntegerField(required=False, min_value=0,
                                error_messages={
                                    'invalid': '`since` is invalid',
