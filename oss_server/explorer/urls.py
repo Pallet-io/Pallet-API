2a87440b34c7d390aa044dcf1f7d7a0190be47ce
lbyyy <skzlbyyy@gmail.com>
2017-11-20 11:28:36 +0800

    Remove Explorer: transactions/color/[:color_id]

--- oss_server/explorer/urls.py
+++ oss_server/explorer/urls.py
@@ -6,7 +6,6 @@ from .v1.views import (GetAddressBalanceView,
                        GetAddressUtxoView,
                        GetBlockByHashView,
                        GetBlockByHeightView,
-                       GetColorTxsView,
                        GetBlocksView,
                        GetTxByHashView)
 
@@ -15,7 +14,6 @@ urlpatterns = [
     url('^v1/blocks/(?P<block_hash>[A-Za-z0-9]{64})', GetBlockByHashView.as_view()),
     url('^v1/blocks/(?P<block_height>\d{1,10})', GetBlockByHeightView.as_view()),
     url('^v1/transactions/(?P<tx_hash>[A-Za-z0-9]{64})', GetTxByHashView.as_view()),
-    url('^v1/transactions/color/(?P<color_id>\d{1,10})', GetColorTxsView.as_view()),
     url('^v1/transactions/address/(?P<address>[13][a-km-zA-HJ-NP-Z1-9]{26,33})', GetAddressTxsView.as_view()),
     url('^v1/addresses/(?P<address>[13][a-km-zA-HJ-NP-Z1-9]{26,33})/balance', GetAddressBalanceView.as_view()),
     url('^v1/addresses/(?P<address>[13][a-km-zA-HJ-NP-Z1-9]{26,33})/op_return', GetAddressOpReturnView.as_view()),
